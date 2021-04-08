0832526d10ac6dd48eae56e58bfa0a702fde8ce7
BorisWinter <borisbenjaminwinter@gmail.com>
2021-04-08 21:20:43 +0200

    Set back MackeyGlass b/c its not used

--- main_MackeyGlass.py
+++ main_MackeyGlass.py
@@ -1,33 +1,27 @@
 '''
 This is an example of the application of DeepESN model for next-step prediction on Mackey Glass time-series. 
-
 Reference paper for DeepESN model:
 C. Gallicchio, A. Micheli, L. Pedrelli, "Deep Reservoir Computing: A Critical Experimental Analysis", 
 Neurocomputing, 2017, vol. 268, pp. 87-99
-
 Reference paper for the design of DeepESN model in multivariate time-series prediction tasks:
 C. Gallicchio, A. Micheli, L. Pedrelli, "Design of deep echo state networks",
 Neural Networks, 2018, vol. 108, pp. 33-47 
     
 ----
-
 This file is a part of the DeepESN Python Library (DeepESNpy)
-
 Luca Pedrelli
 luca.pedrelli@di.unipi.it
 lucapedrelli@gmail.com
-
 Department of Computer Science - University of Pisa (Italy)
 Computational Intelligence & Machine Learning (CIML) Group
 http://www.di.unipi.it/groups/ciml/
-
 ----
 '''
 
 import numpy as np
 import random
 from DeepESN import DeepESN
-from utils import MSE, config_MG, load_MG, config_chest, load_chest, select_indexes
+from utils import MSE, config_MG, load_MG, select_indexes
 class Struct(object): pass
 
 # sistemare indici per IP in config_pianomidi, mettere da un'altra parte
@@ -39,14 +33,14 @@ def main():
    
     # dataset path 
     path = 'datasets'
-    dataset, Nu, error_function, optimization_problem, TR_indexes, VL_indexes, TS_indexes = load_chest(path, MSE)
+    dataset, Nu, error_function, optimization_problem, TR_indexes, VL_indexes, TS_indexes = load_MG(path, MSE)
 
     # load configuration for pianomidi task
-    configs = config_chest(list(TR_indexes) + list(VL_indexes))
+    configs = config_MG(list(TR_indexes) + list(VL_indexes))
     
     # Be careful with memory usage
-    Nr = 10 # number of recurrent units
-    Nl = 10 # number of recurrent layers
+    Nr = 100 # number of recurrent units
+    Nl = 5 # number of recurrent layers
     reg = 0.0;
     transient = 100
     
